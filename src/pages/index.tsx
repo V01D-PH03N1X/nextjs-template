498bf16a65f9553090df101606d2dc17e8d2b152
Sebastian Schindler <87519233+DarkModz-Official@users.noreply.github.com>
2023-07-23 05:33:11 +0200

    #3 adding meta tags (#4)
    * fix: Public Folder is not in Repository.
    :bug: added .placeholder file
    :memo: updated readme file
    
    * feat: Add metaTags to head
    :sparkles: created a possibility to add Metatags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
+import Head from "next/head";
+
 export default function Home() {
+	const metaDescription = "Default Description";
+	const metaAuthor = "Default Author";
+	const metaKeywords = "Default Keywords";
+
 	return (
-		<main>
-			<h1 className='text-4xl font-bold text-center'>Hello World</h1>
-		</main>
+		<>
+			<Head>
+				<title>Next.js + Tailwind CSS</title>
+				<meta name='description' content={metaDescription} />
+				<meta name='author' content={metaAuthor} />
+				<meta name='keywords' content={metaDescription} />
+			</Head>
+			<main>
+				<h1 className='text-4xl font-bold text-center'>
+					{metaDescription}
+				</h1>
+			</main>
+		</>
 	);
 }
